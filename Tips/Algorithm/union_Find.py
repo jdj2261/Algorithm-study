ece29e40bfc303c472294159d60cde5440980069
djjin <wlseowhd12@naver.com>
2021-01-10 23:25:37 +0900

    modify union_Find.py

--- Tips/Algorithm/union_Find.py
+++ Tips/Algorithm/union_Find.py
@@ -5,17 +5,20 @@ Title : Union find
 address : https://m.blog.naver.com/ndb796/221230967614 (동빈나 알고리즘)
 """
 
+# 부모 노드를 찾는 함수
+def getParent(parent, x):
+    if parent[x] == x: return x
+    parent[x] = getParent(parent, parent[x])
+    return parent[x] 
+
+# 두 부모 노드를 합치는 함수
 def unionParent(parent, a, b):
     a = getParent(parent, a)
     b = getParent(parent, b)
     if a < b: parent[b] = a
     else: parent[a] = b
 
-def getParent(parent, x):
-    if parent[x] == x: return x
-    parent[x] = getParent(parent, parent[x])
-    return parent[x] 
-
+# 같은 부모를 가지는지 확인
 def findParent(parent, a, b):
     a = getParent(parent, a)
     b = getParent(parent, b)
