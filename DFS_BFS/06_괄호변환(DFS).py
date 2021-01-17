285f0b0ddad4c353f29654fbe8ea29a212e0816d
djjin <wlseowhd12@naver.com>
2021-01-17 17:42:38 +0900

    update DFS

--- DFS_BFS/06_괄호변환(DFS).py
+++ DFS_BFS/06_괄호변환(DFS).py
@@ -11,3 +11,49 @@ Title : 괄호변환 (DFS)
 1 1 1 0 0 2
 0 0 0 0 0 2
 """
+
+# 균형잡힌 괄호 문자열의 인덱스 반환
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+        
+# 올바른 괄호 문자열인지 판단
+def check_proper(p):
+    count = 0
+    for i in p:
+        if i == "(":
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1 :]
+    if check_proper(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+
