2c8a8e4d67f1a3034a2fcddac0f962ac860ca25f
djjin <wlseowhd12@naver.com>
2021-02-13 00:30:39 +0900

    add graph algorithm

--- Graph/02_도시분할계획.py
+++ Graph/02_도시분할계획.py
@@ -1,12 +1,19 @@
-# 크루스칼 알고리즘 소스코드 
+"""
+Date : 21년 2월 12일
+Description : Algorithm Study
+Title : 도시 분할 계획 (그래프 이론) 
+"""
+
+import sys
+input = sys.stdin.readline
 
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
+def union_parent(parent, a ,b):
+    a = find_parent(parent ,a)
     b = find_parent(parent, b)
 
     if a < b:
@@ -14,26 +21,25 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-v, e = map(int, input().split())
-parent = [0] * (v+1)
+n, m = map(int, input().split())
+parent = [0] * (n+1)
 
-# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
-edges = []
-result = 0
-
-for i in range(1, v+1):
+for i in range(n+1):
     parent[i] = i
 
-for _ in range(e):
+edges = []
+result = 0
+last = 0
+for _ in range(m):
     a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
-
 edges.sort()
 
 for edge in edges:
     cost, a, b = edge
     if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
+        union_parent(parent,a,b)
         result += cost
+        last = cost
 
-print(result)
+print(result - last)
