1d5a41dfb83a619c739ddd4ac1a767836d318dd4
djjin <wlseowhd12@naver.com>
2021-02-20 23:55:04 +0900

    add Implementation 06.py

--- Implementation/second/06_2_문자열재정렬.py
+++ Implementation/second/06_2_문자열재정렬.py
@@ -4,11 +4,23 @@ Description : Algorithm Study 실전문제
 Title : 문자열 재정렬(Implementation) 
 """
 """
-문제 생략
-
 입력예시                출력예시
 K1KA5CB7            ABCKK13
 
 입력예시                출력예시
 AJKDLSI412K4JSJPD   ADDIJJJKKLSS20
 """
+input_data = input()
+
+alpha_list = []
+digit_list = []
+for data in input_data:
+    if data.isalpha():
+        alpha_list.append(data)
+    else:
+        digit_list.append(int(data))
+
+alpha_list.sort()
+result = ''.join(alpha_list) + str(sum(digit_list))
+
+print(result)
