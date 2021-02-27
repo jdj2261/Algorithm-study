bd634f2c25a1fa785c4325f1bac58ea143a24dc9
djjin <wlseowhd12@naver.com>
2021-02-28 00:14:19 +0900

    add DFS_BFS third

---
+++ DFS_BFS/third/01_3_음료수얼려먹기.py
@@ -0,0 +1,42 @@
+"""
+Date : 20년 2월 27일
+Description : Algorithm Study
+Title : 음료수 얼려 먹기
+"""
+"""
+N * M
+구멍 -> 0
+칸막이 -> 1
+입력        출력
+00110       3
+00011
+11111
+00000
+"""
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    count = 0
+    if x <= -1 or x >= n or y <= -1 or y>=m:
+        return False
+    
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+        return True
+    return False
+
+result = 0
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:
+            result += 1
+
+print(result)
