68742d06ec985e088c402f279c567478ade1913f
djjin <wlseowhd12@naver.com>
2021-03-07 14:21:17 +0900

    added DP second

--- BinarySearch/second/02_2_떡볶이떡만들기.py
+++ BinarySearch/second/02_2_떡볶이떡만들기.py
@@ -12,4 +12,19 @@ Title : 떡볶이 떡 만들기(이진탐색)
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
 
+start = 0
+end = max(data)
 
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end)//2
+    for x in data:
+        if x > mid:
+            total += x - mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+print(result)
---
+++ DynamicProgramming/second/02_2_개미전사.py
@@ -0,0 +1,22 @@
+"""
+Date : 20년 3월 7일
+Description : Algorithm Study
+Title : 개미 전사 (다이나믹 프로그래밍) 
+"""
+"""
+입력 예시   출력 예시
+4           8
+1 3 1 5
+"""
+
+n = int(input())
+array = list(map(int, input().split()))
+
+dp = [0] * 100
+
+dp[0] = array[0]
+dp[1] = max(array[0], array[1])
+for i in range(2, n):
+    dp[i] = max(dp[i-1], dp[i-2] + array[i])
+
+print(dp[n-1])
