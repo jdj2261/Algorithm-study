88a19a5ab4fc0477351552906035bd856ecf3450
djjin <wlseowhd12@naver.com>
2021-04-15 00:39:57 +0900

    add BJoon DP

--- BJoon/DP/11722_가장긴감소하는부분수열.py
+++ BJoon/DP/11722_가장긴감소하는부분수열.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 array = list(map(int, input().split()))
-dp = [i for i in array]
+dp = [1] * n
 
 for i in range(1, n):
     for j in range(i):
-        if array[j] < array[i]:
-            dp[i] = max(dp[i], dp[j] + array[i])
+        if array[j] > array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
